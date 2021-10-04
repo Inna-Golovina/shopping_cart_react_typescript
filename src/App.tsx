36e2e79b7e46aca3efa04a87b4c2f64fb4350cf2
Inna Golovina <igigig978@gmail.com>
2021-10-04 16:24:55 +0300

    added ts type, fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+import { Drawer } from "@mui/material";
+import { LinearProgress } from "@mui/material";
+import { Grid } from "@mui/material";
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { Badge } from "@mui/material";
+import { Wrapper } from "./App_styles";
+import { log } from "console";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+  const { data, isLoading, error} = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  console.log(data);
   return (
     <div className="App">
-      
+      Start
     </div>
   );
 }
