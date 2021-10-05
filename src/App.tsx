c3b61c12c5d5a43fa1a9e8143dfedccf4ca84254
Inna Golovina <igigig978@gmail.com>
2021-10-05 12:15:55 +0300

    added Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
-import { Drawer } from "@mui/material";
-import { LinearProgress } from "@mui/material";
-import { Grid } from "@mui/material";
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import { Badge } from "@mui/material";
+import Badge from '@material-ui/core/Badge';
 import { Wrapper } from "./App_styles";
-import { log } from "console";
+
+import Item from "./components/Item/Item";
 
 export type CartItemType = {
   id: number;
@@ -29,10 +30,34 @@ const App = () => {
   );
 
   console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) {
+    return <LinearProgress />
+  }
+
+  if (error) {
+    return <div>Something went wrong...</div>
+  }
+
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item
+              item={item}
+              handleAddToCart={handleAddToCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
